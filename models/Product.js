3b28ecc1132585bcbcad174ba2aa7a2cdbeafaf1
G Codes <graciela.mejia1@yahoo.com>
2022-10-28 00:29:19 -0700

    initial commit/notes in models on requirements to work on/update to server.js with sequelize and server

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,7 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -10,13 +10,18 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    //id column (integer, not null, primary key, auto increment)
+    //product_name column (string, not null)
+    //price column (decimal, not null, validates as decimal)
+    //stock column (integer, not null, set value 10, validates numeric value)
+    //category_id column (integer, references `category` model's `id`)
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,21 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    //id column (integer, not null, primary key, auto increment)
+    //product_id column (integer, references `tag` model's `id`)
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
